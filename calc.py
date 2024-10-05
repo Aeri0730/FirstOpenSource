9eca06fae810e79a76233bbea8e5cc7fa722937d
LJJANG <dodl8549@gmail.com>
2024-10-05 17:41:24 +0900

    feature-b2

--- calc.py
+++ calc.py
@@ -6,11 +6,14 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a,b):
+    return a * b
+
 def main():
-    print("간단한 덧셈/뺄셈 프로그램입니다.")
+    print("간단한 덧셈/뺄셈/곱셈 프로그램입니다.")
     num1 = float(input("첫 번째 숫자를 입력하세요: "))
     num2 = float(input("두 번째 숫자를 입력하세요: "))
-    operation = input("연산을 선택하세요 (+, -): ")
+    operation = input("연산을 선택하세요 (+, -, *): ")
 
     if operation == '+':
         result = add(num1, num2)
@@ -18,8 +21,11 @@ def main():
     elif operation == '-':
         result = subtract(num1, num2)
         print(f"결과: {num1} - {num2} = {result}")
+    elif operation == '*':
+        result = multiply(num1,num2)
+        print(f"결과 : {num1} * {num2} = {result}")
     else:
-        print("잘못된 연산자입니다. +, - 중 하나를 입력하세요.")
+        print("잘못된 연산자입니다. +, -, * 중 하나를 입력하세요.")
  
 
 if __name__ == "__main__":
